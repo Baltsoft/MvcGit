f659f437a06083522ba224488a8f1096e486a3f2
Tomas <tomas@baltsoft.com>
2015-12-11 10:31:01 +0200

    test

---
+++ MvcGit/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcGit.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var test="test"; 
+            return View();
+        }
+
+    }
+}
